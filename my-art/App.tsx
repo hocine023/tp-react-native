c1e1ad5df7c4e82dcf876526031def8a19ea4d19
Hocine Akli <hocine.akli@ecoles-epsi.net>
2026-03-10 15:59:03 +0100

    update

--- my-art/App.tsx
+++ my-art/App.tsx
@@ -1,6 +1,27 @@
 import React from "react";
-import AppNavigator from "./src/navigation/AppNavigator";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailScreen from "./src/screens/DetailScreen";
+import { RootStackParamList } from "./src/types/navigation";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Artworks" }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{ title: "Détail" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
